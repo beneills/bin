7328763b5cc0fde3f97bac860f759ec904b86c6e
Ben Eills <ben@beneills.com>
2013-07-01 15:12:10 +0100

    + teeth beemind

--- daily.sh
+++ daily.sh
@@ -5,3 +5,9 @@
 genplan.rb # prerequisite for emacs
 beemem.rb &
 beeplan.rb
+
+YESTERDAY_PLAN=~/plans/`date --date="yesterday" +%F`.org
+if grep --quiet --ignore-case "DONE morning teeth" $YESTERDAY_PLAN \
+    && grep --quiet --ignore-case "DONE evening teeth" $YESTERDAY_PLAN; then
+    beemind teeth 1 "added by daily.sh"
+fi
